6da9e745b8818e1ca75f5d2c71fa2d0212cec6d2
Colile1 <colilesibanda@gmail.com>
2024-01-03 02:36:11 +0200

    sign for last_int

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-
+number = -626
+sign = number/abs(number) 
 str_number = str(number)
 last_digit = str_number[len(str_number)-1]
-last_int = int(last_digit)
+last_int = int(last_digit)*sign
 if int(last_int) > 5:
     msg = "and is greater than 5"
 elif last_int == 0:
