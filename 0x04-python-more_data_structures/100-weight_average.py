1cd42a631a3b613e2db68705aff8604262db8e08
Colile1 <colilesibanda@gmail.com>
2024-01-11 02:15:03 +0200

    10-12

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list:
-        return sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
-    return 0
+    if not my_list:
+        return (0)
+    sum_products = sum(score * weight for score, weight in my_list)
+    sum_weights = sum(weight for _, weight in my_list)
+    return (sum_products / sum_weights)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
-    p = 0
 
-    if type(roman_string) is str and roman_string:
-        for c in range(len(roman_string) - 1, -1, -1):
-            if val[roman_string[c]] >= p:
-                res += val[roman_string[c]]
-            else:
-                res -= val[roman_string[c]]
-            p = val[roman_string[c]]
-    return res
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or not roman_string:
+        return 0
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = roman_values[char]
+        if value >= prev_value:
+            result += value
+        else:
+            result -= value
+        prev_value = value
+        
+    return (result)
