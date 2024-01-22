922e5c6fc73fe5b428fb9778cb5bfef52e78863a
Colile1 <colilesibanda@gmail.com>
2024-01-23 01:01:02 +0200

    code

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - displays basic info about Python bytes objects
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_bytes(PyObject *p)
+{
+}
+/**
+ * print_python_float - displays Python float
+ * @p: python float
+ * Return: nothing
+ **/
+void print_python_float(PyObject *p)
+{
+}
+/**
+ * print_python_list - displays basic info about Python lists
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_list(PyObject *p)
+{
+}
