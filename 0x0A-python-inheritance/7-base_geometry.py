496865943a1c86e0e2bb32887a8452e77f4a04d1
Colile1 <colilesibanda@gmail.com>
2024-02-06 03:12:39 +0200

    7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,13 +13,13 @@ class BaseGeometry:
         """Validate that a parameter is an integer.
 
         Args:
-            name (str): The name of the parameter.
+            name (str): The name of the parameter. 
             value (int): The parameter to validate.
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name)) 
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
