a005de15cc5961036b1a8db33bdf7f8525a870d6
Colile1 <colilesibanda@gmail.com>
2024-02-20 12:59:59 +0200

    0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -31,7 +31,7 @@ def matrix_divided(matrix, div):
 
     if not isinstance(matrix, list) or not all(
     isinstance(row, list) for row in matrix
-):
+    ):
     raise TypeError(
         "matrix must be a matrix (list of lists) of integers/floats"
     )
