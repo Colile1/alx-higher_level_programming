001293a1f31abe1ab8857824a3c0a70cc338de96
Colile1 <colilesibanda@gmail.com>
2024-03-12 22:48:58 +0200

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 --lists all the tables of a database in a MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (
-  id INT, 
-  name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
