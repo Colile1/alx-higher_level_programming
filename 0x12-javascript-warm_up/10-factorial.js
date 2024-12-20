009416d724a14a2aedcb42a02a88ce53350b197a
Colile1 <colilesibanda@gmail.com>
2024-12-20 10:46:38 +0200

    cleaned up formating

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /**
- * Recursive function to calculate the factorial of a number.
+ * Recursive function to calculate the factorial of a number
  *
- * @param {number} n - The number to calculate the factorial for.
- * @return {number} The factorial of the input number.
+ * @param {number} n Number to calculate the factorial for.
+ * @return {number} Factorial of the input number.
  */
 function factorial (n) {
   return (n === 0 || isNaN(n) ? 1 : n * factorial(n - 1));
