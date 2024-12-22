402ae899951165ceeb9c4052c071dba610970b75
Colile1 <colilesibanda@gmail.com>
2024-12-22 04:13:22 +0200

    s

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This is a bash code that runs all the files in the current file and all sub files
-shopt -s globstar
-
-for file in **; do
-  if [ -f "$file" ]; then
-    bash "$file"
-  fi
-done
--- s.sh
+++
@@ -1,6 +0,0 @@
-Get-ChildItem *.py | ForEach-Object {
-  $_.Attributes = "Archive"
-}
-the ls -l equivalent in powershell
-Get-ChildItem -Path . -Force | Select-Object Mode, LastWriteTime, Length, Name
-
