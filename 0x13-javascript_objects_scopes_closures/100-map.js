ddca8128270b4160563bb4057ca33e1babc9d462
Colile1 <colilesibanda@gmail.com>
2024-12-22 04:30:04 +0200

    4

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,4 @@
 const {list} = require('./100-data.js');
 console.log(list);
 const newList = list.map((value, index) => value * index);
-
-console.log(list);
 console.log(newList);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const contentA = fs.readFileSync(process.argv[2], 'utf8');
-const contentB = fs.readFileSync(process.argv[3], 'utf8');
-const contentC = contentA.concat(contentB);
-fs.writeFileSync(process.argv[4], contentC, 'utf8');
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
