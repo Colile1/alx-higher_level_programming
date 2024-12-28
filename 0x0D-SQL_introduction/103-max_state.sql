a98fcce18c06e415c9c4ba8e7b911567d983709b
Colile1 <colilesibanda@gmail.com>
2024-12-28 16:09:04 +0200

    max temperature of each state (ordered by State name)

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- displays the max temperature of each state (ordered by State name).
 SELECT 
     state,
-    MAX(temperature) AS max_temp
+    MAX(value) AS max_temp
 FROM 
     temperatures
 GROUP BY 
