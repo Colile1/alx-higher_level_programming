2589338c016568f26feb9263a075fda9c084b722
Colile1 <colilesibanda@gmail.com>
2024-12-28 12:26:48 +0200

    script to list all records with a score of 10 or more

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,5 @@
 -- prints all records of the table second_table of the database in MySQL server.
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
