35384c66ee6f654692bb13fd2913a5d12e854379
colile1 <colilesibanda@gmail.com>
2025-03-06 18:39:03 +0200

    script sends a GET request and displays the body only if the status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # This script sends a GET request and displays the body only if the status code is 200.
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+if [ "$#" -ne 1 ]; then
+    exit 1
+fi
+
+response=$(curl -s -w "%{http_code}" "$1")
+
+status="${response: -3}"
+body="${response::-3}"
+
+if [ "$status" -eq 200 ]; then
+    echo "$body"
+fi
