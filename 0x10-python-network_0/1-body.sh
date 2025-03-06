ac44ff4de3fc3914799076dba38b1ba1a31c6851
colile1 <colilesibanda@gmail.com>
2025-03-06 19:26:43 +0200

    0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body if the status code is 200
-output=$(curl -s -w "\n%{http_code}" "$1") && status="${output##*$'\n'}" && body="${output%$'\n'*}" && [ "$status" -eq 200 ] && printf "%s" "$body"
+curl -s "$1"
